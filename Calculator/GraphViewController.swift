7532c62bb7422e7e9f66d588e474113e1392fe25
Nadir Muzaffar <nadirabid@gmail.com>
2015-09-24 10:40:37 -0700

    get split view segue working

---
+++ Calculator/GraphViewController.swift
@@ -0,0 +1,14 @@
+//
+//  GraphViewController.swift
+//  Calculator
+//
+//  Created by Nadir Muzaffar on 9/24/15.
+//  Copyright © 2015 Nadir Muzaffar. All rights reserved.
+//
+
+import UIKit
+
+class GraphViewController: UIViewController {
+    
+
+}
